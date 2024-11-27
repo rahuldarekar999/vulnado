c8cadd1ab2b5e67bdedd5d8322c593639511e0e8
rahuldarekar999 <83153025+rahuldarekar999@users.noreply.github.com>
2024-11-27 16:38:41 +0530

    Update CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -8,6 +8,13 @@ import java.io.Serializable;
 @RestController
 @EnableAutoConfiguration
 public class CowController {
+    String abcd(String input, String inpu2) {
+        return myfunction(input);
+    }
+
+    private String myfunction(String input) {
+        return input;
+    }
     @RequestMapping(value = "/cowsay")
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
         return Cowsay.run(input);
