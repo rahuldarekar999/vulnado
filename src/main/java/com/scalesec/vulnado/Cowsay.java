48918cacdb1333d2b9fe9ea2d1ee6f5dc188cef8
rahuldarekar999 <83153025+rahuldarekar999@users.noreply.github.com>
2024-11-27 14:50:40 +0530

    Update Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -8,7 +8,7 @@ public class Cowsay {
     ProcessBuilder processBuilder = new ProcessBuilder();
     String cmd = "/usr/games/cowsay '" + input + "'";
     System.out.println(cmd);
-    if(mymethod(cmd)){
+    if(valid(cmd)){
       processBuilder.command("bash", "-c", cmd);
     } else {
       return "hello";
@@ -28,7 +28,7 @@ public class Cowsay {
     }
     return output.toString();
   }
-  public boolean mymethod(String abcd){
+  public boolean valid(String abcd){
     return cmd.matches("[a-zA-Z0-9\\-\\_]+");
   }
 }
